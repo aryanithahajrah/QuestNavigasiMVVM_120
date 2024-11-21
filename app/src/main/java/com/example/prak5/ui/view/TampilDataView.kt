873bdeea64a2a3a34fae90c01361f795b5fca308
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-11-21 16:05:55 +0700

    tampil data view digunakan untuk menampilkan detail data siswa

---
+++ app/src/main/java/com/example/prak5/ui/view/TampilDataView.kt
@@ -0,0 +1,37 @@
+package com.example.prak5.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.prak5.model.DataSiswa
+
+
+@Composable
+fun TampilDataView(
+    uiState: DataSiswa,
+    onBackButton: () -> Unit
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        TampilData(parameterNama = "Nama", argum = uiState.nama)
+        TampilData(parameterNama = "NIM", argum = uiState.nim)
+        TampilData(parameterNama = "Jenis Kelamin", argum = uiState.gender)
+        TampilData(parameterNama = "Alamat", argum = uiState.alamat)
+        TampilData(parameterNama = "No Telephone", argum = uiState.notelephone)
+        Button(onClick = onBackButton) {
+
+            Text(text = "Kembali")
+        }
+    }
+}

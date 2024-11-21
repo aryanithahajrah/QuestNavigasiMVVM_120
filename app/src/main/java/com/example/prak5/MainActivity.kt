f2da79fa863d1073953d4a10cf893fdb8bd241d4
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-11-21 16:16:24 +0700

    digunakan untuk menampilkan teks di layar

--- app/src/main/java/com/example/prak5/MainActivity.kt
+++ app/src/main/java/com/example/prak5/MainActivity.kt
@@ -29,3 +29,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+

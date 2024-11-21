f0f846b9c6795918cfe9e50b0f265d2e66d33f7f
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-11-21 16:02:08 +0700

    data dummy yang mewakili jenis kelamin

---
+++ app/src/main/java/com/example/prak5/model/JenisKelamin.kt
@@ -0,0 +1,11 @@
+package com.example.prak5.model
+
+import com.example.prak5.R
+
+//membuat dummy data
+object jenisKelamin {
+    val JenisKelamin = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
